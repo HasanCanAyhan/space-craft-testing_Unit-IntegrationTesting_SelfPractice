2210b4ca7cac37f053eb18eee19916022492c369
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-12-20 18:27:53 +0100

    performed integration testing for GameControllerIT

--- src/test/java/com/cydeo/spacecraft/integration/controller/GameControllerIT.java
+++ src/test/java/com/cydeo/spacecraft/integration/controller/GameControllerIT.java
@@ -1,9 +1,13 @@
 package com.cydeo.spacecraft.integration.controller;
 
+import com.cydeo.spacecraft.entity.Game;
+import com.cydeo.spacecraft.enumtype.AttackType;
 import com.cydeo.spacecraft.enumtype.Boost;
 import com.cydeo.spacecraft.enumtype.Level;
 import com.cydeo.spacecraft.model.request.CreateGameRequest;
+import com.cydeo.spacecraft.model.request.CreateHitRequest;
 import com.cydeo.spacecraft.model.response.CreateGameResponse;
+import com.cydeo.spacecraft.repository.GameRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -12,10 +16,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,18 +35,26 @@ public class GameControllerIT {
     @Autowired
     private ObjectMapper objectMapper;// to convert object to JSON
 
+    @Autowired
+    private GameRepository gameRepository;
+
+
     @Test
     public void
     should_create_game_successfully() throws Exception {
 
+        //sample object
         CreateGameRequest createGameRequest = new CreateGameRequest();
         createGameRequest.setUsername("username");
         createGameRequest.setBoost(Boost.BIG_BOMB);
         createGameRequest.setLevel(Level.EASY);
 
+        //make a http request to specific
         MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/api/v1/game/createGame")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsBytes(createGameRequest)))
+
+                //checking assertions
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.gameId").exists())
                 .andExpect(jsonPath("$.responseMessage").value("SUCCESS"))
@@ -49,9 +63,34 @@ public class GameControllerIT {
         String json = result.getResponse().getContentAsString();
         System.out.println(json);
         CreateGameResponse createGameResponse = objectMapper.readValue(json,CreateGameResponse.class);
-
         System.out.println(createGameResponse.getGameId());
 
+        //checking if game is really exist in the DB
+        Game game = gameRepository.findById(createGameResponse.getGameId()).orElseThrow();
+        assertEquals(game.getIsEnded(), false);
+        assertEquals(game.getBoost(), createGameRequest.getBoost());
+        assertEquals(game.getLevel(),createGameRequest.getLevel());
+
     }
+
+    @Test
+    @Sql(scripts = "/sql/hit_and_player_win.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = "/sql/remove_data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void should_player_win_if_player_attack_to_target() throws Exception {
+        CreateHitRequest createHitRequest = new CreateHitRequest();
+        createHitRequest.setAttackType(AttackType.PLAYER_TO_TARGET);
+        createHitRequest.setGameId(1L);
+
+        //make a http request to specific
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/api/v1/game/createHit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(createHitRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.responseMessage").value("SUCCESS"))
+                .andReturn();
+
+
+    }
+
 
 }
